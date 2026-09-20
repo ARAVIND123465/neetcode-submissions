b4b8f646d453cab8ec0b13a9de56f572c78c65fa
Aravindhan S <aravindsuresh5338@gmail.com>
2026-09-20 23:53:23 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+          HashMap<Integer, Integer> res = new HashMap<>();
+
+        for(int i = 0; i < nums2.length; i++) {
+
+            int greater = -1;
+
+            for(int j = i + 1; j < nums2.length; j++) {
+
+                if(nums2[j] > nums2[i]) {
+                    greater = nums2[j];
+                    break;
+                }
+            }
+
+            res.put(nums2[i], greater);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            ans[i] = res.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
